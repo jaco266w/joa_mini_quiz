e1ebb5157eddf9a1d7f6c67278e5e6bf7453b73d
jaco266w <jaco266w@stud.kea.dk>
2023-11-14 16:06:07 +0100

    modal

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -1,23 +1,32 @@
+"use client";
+
 import Image from "next/image";
+import Modal from "./Modal";
 
 export default function QuizCard() {
 	return (
-		<div className="card w-96 shadow-xl bg-accent">
-			<figure>
-				<Image
-					className="object-cover h-56"
-					width={500}
-					height={500}
-					src="/quiz.png"
-					alt="Shoes"
-				/>
-			</figure>
-			<div className="card-body">
-				<p className="uppercase">Hjælp til gaven?</p>
-				<h2 className="card-title font-serif capitalize text-2xl">
-					Gaven til den eneste ene
-				</h2>
+		<>
+			<div
+				className="card w-96 shadow-xl bg-accent"
+				onClick={() => document.getElementById("my_modal_3").showModal()}
+			>
+				<figure>
+					<Image
+						className="object-cover h-56"
+						width={500}
+						height={500}
+						src="/quiz.png"
+						alt="Shoes"
+					/>
+				</figure>
+				<div className="card-body">
+					<p className="uppercase">Hjælp til gaven?</p>
+					<h2 className="card-title font-serif capitalize text-2xl">
+						Gaven til den eneste ene
+					</h2>
+				</div>
 			</div>
-		</div>
+			<Modal />
+		</>
 	);
 }
