1e9527cd54f0046c4e96201c924e70240e93327f
jaco266w <jaco266w@stud.kea.dk>
2023-11-14 15:13:53 +0100

    trusted images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['images.matas.dk'],
+      },
+}
 
 module.exports = nextConfig
