7ba0ec3c0e7458ea9447b5cc79ec0528bd97bcee
Anders Lindberg <anderslindberg999@gmail.com>
2023-11-17 09:50:32 +0100

    hej hej

--- src/components/Qstart.jsx
+++ src/components/Qstart.jsx
@@ -1,8 +1,12 @@
+import Image from "next/image";
+
 export default function Qstart() {
   return (
     <div>
-      <div>
-        <h2>Find din perfekte gave</h2>
+      <div className="grid place-content-center">
+        <h2 className="font-serif text-center font-bold text-3xl">Find din perfekte gave</h2>
+        <div className=" h-96 bg-center bg-[url('/quiz.png')] w-96 shadow-xl mix-blend-normal"></div>
+        {/* <Image className="w-fit h-fit outline-none" src="/quiz.png" alt="produkt" width={500} height={500} /> */}
       </div>
     </div>
   );
