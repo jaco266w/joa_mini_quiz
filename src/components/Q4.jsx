0dde96ac25610bd44fb2773e338d5134a73a1482
Oliver G.A <hi@oliverga.com>
2023-11-17 10:24:48 +0100

    slider

--- src/components/Q4.jsx
+++ src/components/Q4.jsx
@@ -1,23 +1,30 @@
-"use client";
+import React, { useState } from "react";
 
 export default function Q4() {
-	return (
-		<>
-			<input
-				type="range"
-				min={0}
-				max="100"
-				value="25"
-				className="range"
-				step="25"
-			/>
-			<div className="w-full flex justify-between text-xs px-2">
-				<span>|</span>
-				<span>|</span>
-				<span>|</span>
-				<span>|</span>
-				<span>|</span>
-			</div>
-		</>
-	);
+  const [value, setValue] = useState(25);
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div className="flex flex-col gap-2 z-50">
+      <input
+        type="range"
+        min={0}
+        max="100"
+        value={value}
+        onChange={handleChange}
+        className="range"
+        step="25"
+      />
+      <div className="w-full flex justify-between text-xs px-2">
+        <span>|</span>
+        <span>|</span>
+        <span>|</span>
+        <span>|</span>
+        <span>|</span>
+      </div>
+    </div>
+  );
 }
