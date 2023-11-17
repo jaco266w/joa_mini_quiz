1576f0762c69003a0a6582b09a01b6584f08d534
Oliver G.A <hi@oliverga.com>
2023-11-17 11:45:46 +0100

    steps

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -3,24 +3,16 @@ export default function Steps(props) {
     <ul className="steps grid place-content-center ">
       <li
         className={`step ${props.currentStep >= 1 ? "step step-primary" : ""}`}
-      >
-        1
-      </li>
+      ></li>
       <li
         className={`step ${props.currentStep >= 2 ? "step step-primary" : ""}`}
-      >
-        2
-      </li>
+      ></li>
       <li
         className={`step ${props.currentStep >= 3 ? "step step-primary" : ""}`}
-      >
-        3
-      </li>
+      ></li>
       <li
         className={`step ${props.currentStep >= 4 ? "step step-primary" : ""}`}
-      >
-        4
-      </li>
+      ></li>
     </ul>
   );
 }
