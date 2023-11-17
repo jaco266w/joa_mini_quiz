b1c3a1dab59dac22ca5c302c5f61cab5d6144847
Oliver G.A <hi@oliverga.com>
2023-11-17 09:21:16 +0100

    4 columns

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,25 +3,35 @@ import ProductCard from "./ProductCard";
 import QuizCard from "./QuizCard";
 
 let { data: produkter, error } = await supabase
-    .from("produkter")
-    .select("*")
-    .order("id", { ascending: true })
-    .limit(11);
-
-
+  .from("produkter")
+  .select("*")
+  .order("id", { ascending: true })
+  .limit(11);
 
 function ProductList(props) {
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {produkter.slice(0, 2).map((produkt) => (
-                <ProductCard name={produkt.name} brand={produkt.brand} image={produkt.image} price={produkt.price} key={produkt.id}/>
-            ))}
-            <QuizCard />
-            {produkter.slice(2).map((produkt) => (
-                <ProductCard name={produkt.name} brand={produkt.brand} image={produkt.image} price={produkt.price} key={produkt.id}/>
-            ))}
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-auto">
+      {produkter.slice(0, 2).map((produkt) => (
+        <ProductCard
+          name={produkt.name}
+          brand={produkt.brand}
+          image={produkt.image}
+          price={produkt.price}
+          key={produkt.id}
+        />
+      ))}
+      <QuizCard />
+      {produkter.slice(2).map((produkt) => (
+        <ProductCard
+          name={produkt.name}
+          brand={produkt.brand}
+          image={produkt.image}
+          price={produkt.price}
+          key={produkt.id}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ProductList;
