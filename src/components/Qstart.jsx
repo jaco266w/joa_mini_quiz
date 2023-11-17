e11965159f40b9d3017820e32141a53abb077622
Oliver G.A <hi@oliverga.com>
2023-11-17 10:58:25 +0100

    images updatez

--- src/components/Qstart.jsx
+++ src/components/Qstart.jsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+
 export default function Qstart() {
   return (
     <div>
-      <div className="grid place-content-center">
-        <h2 className="font-serif text-center font-black text-4xl">Find din perfekte gave</h2>
-        <div className=" h-96 bg-center bg-[url('/gifts.png')] bg-no-repeat w-96  mix-blend-normal"></div>
+      <div className="flex flex-col items-center gap-10">
+        <h2 className="font-serif text-center font-bold text-4xl">
+          Find din perfekte gave
+        </h2>
+        <Image
+          src={"/gifts.png"}
+          alt="hello"
+          width="350"
+          height="350"
+          className="  mix-blend-color-burn grayscale contrast-125  brightness-50"
+        />
       </div>
     </div>
   );
--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,7 +1,7 @@
 export default function ModalButtons(props) {
   return (
     <div className="flex justify-center gap-4 mt-4">
-      <button onClick={props.clickForwards} className="btn btn-primary">
+      <button onClick={props.clickForwards} className="btn btn-primary ">
         Start
       </button>
     </div>
