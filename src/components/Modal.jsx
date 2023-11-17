9abb5ecee64423e684129923c5fbf26ce45d2a1c
jaco266w <jaco266w@stud.kea.dk>
2023-11-17 10:30:18 +0100

    buttons removed slide 5

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -15,83 +15,84 @@ import Qend from "./Qend";
 import Image from "next/image";
 
 export default function Modal() {
-  const [currentSlide, setCurrentSlide] = useState(0);
+	const [currentSlide, setCurrentSlide] = useState(0);
 
-  function nextSlide() {
-    if (currentSlide === 5) {
-      return;
-    }
-    setCurrentSlide((old) => old + 1);
-  }
+	function nextSlide() {
+		if (currentSlide === 5) {
+			return;
+		}
+		setCurrentSlide((old) => old + 1);
+	}
 
-  function prevSlide() {
-    if (currentSlide === 0) {
-      return;
-    }
-    setCurrentSlide((old) => old - 1);
-  }
-  return (
-    <dialog id="my_modal_3" className="modal ">
-      <div className="modal-box h-[80vh] max-w-2xl overflow-clip flex flex-col justify-between rounded-2xl">
-        <Image
-          height={500}
-          width={500}
-          src="/quiz_bg.png"
-          alt="quiz_bg"
-          className="absolute -z-50 top-0 left-0 w-full h-full scale-110"
-        />
-        <form className="" method="dialog">
-          {/* if there is a button in form, it will close the modal */}
-          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-            ✕
-          </button>
-        </form>
-        {currentSlide === 0 && (
-          <>
-            <Qstart />
-          </>
-        )}
-        {currentSlide === 0 && (
-          <>
-            <StartButton clickForwards={nextSlide} />
-          </>
-        )}
-        {currentSlide === 1 && (
-          <Slide title="Første Spørgsmål">
-            <Q1 />
-          </Slide>
-        )}
-        {currentSlide === 2 && (
-          <Slide title="Andet Spørgsmål">
-            <Q2 />
-          </Slide>
-        )}
-        {currentSlide === 3 && (
-          <Slide title="Tredje Spørgsmål">
-            <Q3 />
-          </Slide>
-        )}
-        {currentSlide === 4 && (
-          <Slide title="Fjerde Spørgsmål">
-            <Q4 />
-          </Slide>
-        )}
-        {currentSlide === 5 && (
-          <div>
-            <Qend />
-            <GiftButton />
-          </div>
-        )}
-        {currentSlide !== 0 && (
-          <div clas>
-            <Steps currentStep={currentSlide} />
-            <ModalButtons
-              clickBackwards={prevSlide}
-              clickForwards={nextSlide}
-            />
-          </div>
-        )}
-      </div>
-    </dialog>
-  );
+	function prevSlide() {
+		if (currentSlide === 0) {
+			return;
+		}
+		setCurrentSlide((old) => old - 1);
+	}
+	return (
+		<dialog id="my_modal_3" className="modal ">
+			<div className="modal-box h-4/6 max-w-2xl overflow-clip flex flex-col justify-between rounded-2xl">
+				<Image
+					height={500}
+					width={500}
+					src="/quiz_bg.png"
+					alt="quiz_bg"
+					className="absolute -z-50 top-0 left-0 w-full h-full scale-110"
+				/>
+				<form className="" method="dialog">
+					{/* if there is a button in form, it will close the modal */}
+					<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+						✕
+					</button>
+				</form>
+				{currentSlide === 0 && (
+					<>
+						<Qstart />
+					</>
+				)}
+				{currentSlide === 0 && (
+					<>
+						<StartButton clickForwards={nextSlide} />
+					</>
+				)}
+				{currentSlide === 1 && (
+					<Slide title="Første Spørgsmål">
+						<Q1 />
+					</Slide>
+				)}
+				{currentSlide === 2 && (
+					<Slide title="Andet Spørgsmål">
+						<Q2 />
+					</Slide>
+				)}
+				{currentSlide === 3 && (
+					<Slide title="Tredje Spørgsmål">
+						<Q3 />
+					</Slide>
+				)}
+				{currentSlide === 4 && (
+					<Slide title="Fjerde Spørgsmål">
+						<Q4 />
+					</Slide>
+				)}
+				{currentSlide === 5 && (
+					<div>
+						<Qend />
+						<GiftButton />
+					</div>
+				)}
+				{currentSlide !== 0 ||
+					(currentSlide !== 5 && (
+						<div clas>
+							<Steps currentStep={currentSlide} />
+							<ModalButtons
+								clickBackwards={prevSlide}
+								clickForwards={nextSlide}
+							/>
+						</div>
+					))}
+			</div>
+		</dialog>
+	);
 }
