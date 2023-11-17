f84bab3fbff602805ea30d7132ccaf07bbd43130
jaco266w <jaco266w@stud.kea.dk>
2023-11-17 10:57:07 +0100

    q4

--- src/components/Q4.jsx
+++ src/components/Q4.jsx
@@ -1,30 +1,30 @@
 import React, { useState } from "react";
 
 export default function Q4() {
-  const [value, setValue] = useState(25);
+	const [value, setValue] = useState(25);
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
+	const handleChange = (event) => {
+		setValue(event.target.value);
+	};
 
-  return (
-    <div className="flex flex-col gap-2 z-50">
-      <input
-        type="range"
-        min={0}
-        max="100"
-        value={value}
-        onChange={handleChange}
-        className="range"
-        step="25"
-      />
-      <div className="w-full flex justify-between text-xs px-2">
-        <span>|</span>
-        <span>|</span>
-        <span>|</span>
-        <span>|</span>
-        <span>|</span>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-2 z-50">
+			<input
+				type="range"
+				min={0}
+				max="100"
+				value={value}
+				onChange={handleChange}
+				className="range"
+				step="25"
+			/>
+			<div className="w-full flex justify-between text-xs px-2">
+				<span>0 dkk.</span>
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+			</div>
+		</div>
+	);
 }
