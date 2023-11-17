7ee7f35ab13b6ca9c84dc77f5427ac72f7cb1e0f
Anders Lindberg <anderslindberg999@gmail.com>
2023-11-17 10:37:34 +0100

    qend

--- src/components/Qend.jsx
+++ src/components/Qend.jsx
@@ -2,7 +2,7 @@ export default function Qend() {
   return (
     <div>
       <div className="grid place-content-center">
-        <h2 className="font-serif text-center font-black text-4xl">Se muligheder</h2>
+        <h2 className="font-serif text-center font-black text-4xl">Færdig!</h2>
         <div className=" h-96 bg-center bg-[url('/gifts.png')] bg-no-repeat w-96  mix-blend-normal"></div>
       </div>
     </div>
