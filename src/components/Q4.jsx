e880e065c1cd0a59053acdac2fe90bdb5452cb1c
jaco266w <jaco266w@stud.kea.dk>
2023-11-17 10:02:06 +0100

    slider

--- src/components/Q4.jsx
+++ src/components/Q4.jsx
@@ -1,7 +1,21 @@
 export default function Q4() {
 	return (
-		<ul>
-			<li>test 4</li>
-		</ul>
+		<div className="my-12">
+			<input
+				type="range"
+				min={0}
+				max="100"
+				value="25"
+				className="range"
+				step="25"
+			/>
+			<div className="w-full flex justify-between text-xs px-2">
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+				<span>|</span>
+			</div>
+		</div>
 	);
 }
