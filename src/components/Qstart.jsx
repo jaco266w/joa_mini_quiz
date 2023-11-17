39595d6a0e823815b89a3346707fe1a8143cfb31
Anders Lindberg <anderslindberg999@gmail.com>
2023-11-17 09:53:49 +0100

    qstart pic

--- src/components/Qstart.jsx
+++ src/components/Qstart.jsx
@@ -5,7 +5,7 @@ export default function Qstart() {
     <div>
       <div className="grid place-content-center">
         <h2 className="font-serif text-center font-bold text-3xl">Find din perfekte gave</h2>
-        <div className=" h-96 bg-center bg-[url('/quiz.png')] w-96 shadow-xl mix-blend-normal"></div>
+        <div className=" h-96 bg-center bg-[url('/quiz.png')] w-96  mix-blend-normal"></div>
         {/* <Image className="w-fit h-fit outline-none" src="/quiz.png" alt="produkt" width={500} height={500} /> */}
       </div>
     </div>
