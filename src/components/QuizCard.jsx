77bb4a15ac8129d17fd418dfa02e11e58ccf7a63
Oliver G.A <hi@oliverga.com>
2023-11-17 09:37:34 +0100

    quizcard

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -6,13 +6,24 @@ import Modal from "./Modal";
 export default function QuizCard() {
   return (
     <>
-      <div className="card w-72 bg-neutral cursor-pointer hover:shadow-xl" onClick={() => document.getElementById("my_modal_3").showModal()}>
+      <div
+        className="card w-72 bg-neutral cursor-pointer hover:shadow-xl"
+        onClick={() => document.getElementById("my_modal_3").showModal()}
+      >
         <figure>
-          <Image className="object-cover h-56" width={500} height={500} src="/quiz.png" alt="gaveide" />
+          <Image
+            className="object-cover h-56 scale-105"
+            width={500}
+            height={500}
+            src="/quiz.png"
+            alt="gaveide"
+          />
         </figure>
         <div className="card-body">
           <p className="uppercase text-center">Hjælp til gaven?</p>
-          <h2 className="card-title font-serif capitalize text-xl">Gaven til den eneste ene</h2>
+          <h2 className="card-title font-serif capitalize text-xl">
+            Gaven til den eneste ene
+          </h2>
         </div>
       </div>
       <Modal />
