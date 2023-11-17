4f1a23036c22e3d3bf2ff6749c0663363441d8bb
Anders Lindberg <anderslindberg999@gmail.com>
2023-11-17 09:25:44 +0100

    qstarty

---
+++ src/components/Qstart.jsx
@@ -0,0 +1,9 @@
+export default function Qstart() {
+  return (
+    <div>
+      <div>
+        <h2>Find din perfekte gave</h2>
+      </div>
+    </div>
+  );
+}
--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -4,6 +4,7 @@ export default function Steps(props) {
       <li className={`step ${props.currentStep >= 0 ? "step step-primary" : ""}`}>Fly to moon</li>
       <li className={`step ${props.currentStep >= 1 ? "step step-primary" : ""}`}>Shrink the moon</li>
       <li className={`step ${props.currentStep >= 2 ? "step step-primary" : ""}`}>Grab the moon</li>
+      <li className={`step ${props.currentStep >= 3 ? "step step-primary" : ""}`}>Grab the moon</li>
     </ul>
   );
 }
