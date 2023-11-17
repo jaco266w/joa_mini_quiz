d81b85ce3b1b9c0c300ecb9ff98bfae6c5204bf7
Anders Lindberg <anderslindberg999@gmail.com>
2023-11-17 09:57:33 +0100

    font

--- src/components/Qstart.jsx
+++ src/components/Qstart.jsx
@@ -2,7 +2,7 @@ export default function Qstart() {
   return (
     <div>
       <div className="grid place-content-center">
-        <h2 className="font-serif text-center font-bold text-3xl">Find din perfekte gave</h2>
+        <h2 className="font-serif text-center font-black text-4xl">Find din perfekte gave</h2>
         <div className=" h-96 bg-center bg-[url('/gifts.png')] bg-no-repeat w-96  mix-blend-normal"></div>
       </div>
     </div>
