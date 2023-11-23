0001638c921f8d309c4f839b4c265892dc2c9106
Oliver G.A. <hi@oliverga.com>
2023-11-23 10:33:48 +0100

    end screen

--- src/app/resultspage/page.jsx
+++ src/app/resultspage/page.jsx
@@ -1,21 +1,31 @@
 import ProductCard from "@/components/ProductCard";
 import { supabase } from "@/utils/supabaseClient";
-let { data: produkter, error } = await supabase.from("produkter").select("*").order("id", { ascending: false }).limit(4);
+let { data: produkter, error } = await supabase
+  .from("produkter")
+  .select("*")
+  .order("id", { ascending: false })
+  .limit(4);
 
 export default function Resultspage() {
   return (
-    <div className="flex flex-col bg-neutral justify-items-center min-h-screen">
-      <div className=" grid place-content-center mt-32">
-        <h2 className="text-9xl font-serif font-black text-center mb-32">Gaveideer</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-auto">
-          {produkter.slice(0, 2).map((produkt) => (
-            <ProductCard name={produkt.name} brand={produkt.brand} image={produkt.image} price={produkt.price} key={produkt.id} />
-          ))}
-
-          {produkter.slice(2).map((produkt) => (
-            <ProductCard name={produkt.name} brand={produkt.brand} image={produkt.image} price={produkt.price} key={produkt.id} />
-          ))}
-        </div>
+    <div className="flex flex-col justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center h-32">
+        <h2 className="text-4xl font-serif font-medium text-center ">
+          Gaveideer
+        </h2>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 mx-auto">
+        {produkter.slice(0, 3).map((produkt, index) => (
+          <div key={index} style={{ marginTop: `${2 * index}rem` }}>
+            <ProductCard
+              name={produkt.name}
+              brand={produkt.brand}
+              image={produkt.image}
+              price={produkt.price}
+              key={produkt.id}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
